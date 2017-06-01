c3720fd967c035f982a57640f5c96f9d0f968501
mahamadousylla <msylla@uci.edu>
2017-06-01 00:11:14 -0700

    using render_template

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
-from flask import Flask, send_from_directory, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def search():
-    return app.send_static_file("index.html")
+    return render_template("index.html")
 
 @app.route("/result", methods=['POST']) 
 def getResult():
 	search = request.form['query'].strip()
 	print("made it", search)
-	return app.send_static_file("result.html")
+	return render_template("result.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
