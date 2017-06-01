1bff29ecb7f8bf47ff25a4cea2bfa06b0bc7909a
mahamadousylla <msylla@uci.edu>
2017-06-01 00:42:42 -0700

    added logo

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__, static_url_path='')
+pq = []
+
 
 @app.route("/")
 def search():
@@ -8,9 +10,10 @@ def search():
 
 @app.route("/result", methods=['POST']) 
 def getResult():
+	#pq should be top 10 results
 	search = request.form['query'].strip()
 	print("made it", search)
-	return render_template("result.html")
+	return render_template("result.html", data = pq)
 
 if __name__ == "__main__":
     app.run(debug=True)
