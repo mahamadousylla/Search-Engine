8e3b557be40e06cb988e30b66cbb62a5a55a3516
mahamadousylla <msylla@uci.edu>
2017-06-03 15:29:13 -0700

    edited ranking file

--- views/ranking.py
+++ views/ranking.py
@@ -3,16 +3,16 @@ from math import log                ## used to calculate tf-idf
 from bs4 import BeautifulSoup       ## parse url
 from collections import defaultdict ## a dictionary to keep track of data
 
-path = "C:\Users\Testing\Desktop\WEBPAGES_CLEAN"
+path = "C:\Users\Testing\Desktop\WEBPAGES_CLEAN"    ## Path based on users directory
 allDirectories = []                 ## contains all directory
 allFiles = []                       ## contains all file in the directory
 numOfDocuments = 0                  ## counts the number of document
-BKdictionary = dict()               ## Contains all information from the bookkeeper file
-countDictionary = defaultdict(int)  ## Contanis the number of term in a document
-tfidfDictionary = dict()            ## Contains the calculated tf-idf
+BKdictionary = dict()               ## contains all information from the bookkeeper file
+countDictionary = defaultdict(int)  ## contanis the number of term in a document
+tfidfDictionary = defaultdict(int)  ## contains the calculated tf-idf
 invertedIndex = defaultdict(lambda : defaultdict(int))                        ## An inner and outer dictionary 
 invalidTags = ['body', 'title', 'h1', 'h2', 'h3', 'b', 'strong', 'html', 'p'] ## Invalid tags in url
-
+invalidIndex = ['the', 'a', 'an', 'this', 'that', 'these', 'those']           ## Used to improve searches
 
 class Parser:
     def __init__(self, filePath, dirNum, fileNum):
@@ -33,27 +33,27 @@ class Parser:
                 ## Strips the tags    
                 self.soup = BeautifulSoup(f, 'lxml')
                 for tag in invalidTags:
-                        for match in self.soup.findAll(tag):
-                                match.replaceWithChildren()
+                    for match in self.soup.findAll(tag):
+                        match.replaceWithChildren()
 
                 ## splits the words into a list
                 self.soup = re.split('[^a-zA-Z0-9]', str(self.soup))
                 
                 ## loop through the list and add unique words into dictionary
                 for word in self.soup:
-                        if word != "":
-                            countDictionary[self.doc] += 1
-                            invertedIndex[word.lower()][self.doc] += 1
-                            
-                                                    
+                    if word != "":
+                        countDictionary[self.doc] += 1
+                        invertedIndex[word.lower()][self.doc] += 1                                  
         except:
-                print("You did not enter a valid file path")
+            print("You did not enter a valid file path")
         
         
 def getBKdictionary():
+    global BKdictionary
+    ## Path based on users directory
     with open('C:\Users\Testing\Desktop\WEBPAGES_CLEAN\\bookkeeping.json', "r") as f:
 
-        ## cleans the word so that it is easily placed into the dictionary
+        ## cleans the line so that it is easily placed into the dictionary
         for l in f:
             l = l.strip('\n')
             l = l.strip(',')
@@ -61,124 +61,66 @@ def getBKdictionary():
             l = l.replace('"', '')
             line = l.split(':')
             BKdictionary[line[0]] = line[-1]
-            
-    f.close()
 
 
 def getAllFiles():
-        global BKdictionary
-        global invertedIndex
-        global allDirectories
-        global allFiles
-        global numOfDocuments
-        global countDictionary
-        global tfidfDictionary
+    global BKdictionary
+    global invertedIndex
+    global allDirectories
+    global allFiles
+    global numOfDocuments
+    global countDictionary
+    global tfidfDictionary
 
-## remove file from list, since it is not a directory
-        allDirectories.extend(os.listdir(path))
-        allDirectories.remove('bookkeeping.json')
-        allDirectories.remove('bookkeeping.tsv')
+    ## remove file from list, since it is not a directory
+    allDirectories.extend(os.listdir(path))
+    allDirectories.remove('bookkeeping.json')
+    allDirectories.remove('bookkeeping.tsv')
 
-        getBKdictionary()
+    getBKdictionary()
 
-##        for d in allDirectories: ## 0-74 + json files
-        for d in range(0,30):
-            if os.path.isdir(path + "\\" + str(d) + "\\"):
-                fileDirectory = os.listdir(path + '\\' + str(d))                        
-                numOfDocuments += len(fileDirectory)
+    for d in allDirectories: ## 0-74 + json files
+        if os.path.isdir(path + "\\" + str(d) + "\\"):
+            fileDirectory = os.listdir(path + '\\' + str(d))                        
+            numOfDocuments += len(fileDirectory)
+                    
+        for f in fileDirectory:
+            filePath = path + '\\' + str(d) + '\\' + str(f)
+            parser = Parser(filePath, d, f)
+            parser.countWords()    
 
-                        
-##            for f in fileDirectory:
-            for f in range(0, 30):
-                    filePath = path + '\\' + str(d) + '\\' + str(f)
-                    parser = Parser(filePath, d, f)
-                    parser.countWords()
-
-def writeToFile():
-        textFile = open("C:\Users\Testing\Desktop\invertedIndex.txt", "w")
-
-        ##loop through invertedIndex and write to text file
-        for k, v in invertedIndex.items(): ## loop through outter dictionary (terms)
-            textFile.write('{k} - '.format(k=k))
-            for k2, v2 in v.items(): ## loop through inner dictionary (doc: tf)
-                textFile.write('{k2}: {v2}; '.format(k2 = k2, v2 = v2))
-            textFile.write('\n')
-
-        textFile.close()
-
-def getQuery():
-    
-    textFile = open("C:\Users\Testing\Desktop\Query.txt", "w")
-    textFile.write('Tony Hua 16788298\nMahamadou Sylla 61549479\n\n')
-##    listOfTerms = ['informatics', 'irvine', 'mondego']
-    listOfTerms = []
-##    print('Deliverables: ')
-
-## query for terms
-    ## loop through list of terms
-    for term in listOfTerms:
-        numOfTerm = 0
-##        ## write to file
-##        textFile.write('--------------------------------------------------------------------------\n')    
-##        textFile.write('--------------------------------------------------------------------------\n')
-##        textFile.write('{term}:\n'.format(term = term))
-##        textFile.write('--------------------------------------------------------------------------\n')
-##        textFile.write('--------------------------------------------------------------------------\n')
-
-        ## loop through the inner dictionary of the term
-        for k, v in invertedIndex[term].items():
-            numOfTerm += v
-
-
-##        textFile.write('{key}: {value}\n'.format(key = BKdictionary[k], value = v))
-##        textFile.write('\n')
-##        print('Total number of {term}: {n}'.format(term = term, n = numOfTerm))
-
-##    print('Number of Documents: {n}\n'.format(n = numOfDocuments))
-##    print('Number of Unique Words: {n}\n'.format(n = len(invertedIndex)))
-##    print('Total size(in KB): 88334 KB\n')
-
-        textfile.close()    
-    
 
 def Calculate():
+    termList = []
     term = raw_input('Enter a term: ').lower()
-##    print ('testing...', term.split())
-##    termList = term.split('')
-##    print 'termsList : ', termList
 
-    print '---------------------------'
-    print '---------------------------'
-    print 'Here are the results for {term}'.format(term = term)
-    print '---------------------------'
-    print '---------------------------'
-
+    ## Check if user input one or more value
+    ## Then take the appropriate actions
+    if ' ' in term:
+        termList = term.split(' ')
+        for t in invalidIndex:
+            if t in termList:
+                termList.remove(t)
+    else:
+        termList.append(term)
     
     ## Calculate tf-idf
-    for k, v in invertedIndex[term].items(): ## k = doc, v = tf
-        tf = float(v) / countDictionary[k]            ## countDictionary[k] = total terms in doc
-        idf = log( numOfDocuments, len(invertedIndex[term]) )
-        tf_idf = tf * idf
-        tfidfDictionary[k] = round(tf_idf, 5)
-
+    for t in termList:
+        for k, v in invertedIndex[t].items():    ## k = doc, v = tf
+            tf = float(v) / countDictionary[k]   ## countDictionary[k] = total terms in doc
+            idf = log( numOfDocuments, len(invertedIndex[t]) )
+            tf_idf = tf * idf
+            tfidfDictionary[k] += round(tf_idf, 5)
     
     i = 0 ## keeps track of top 10 value
     for k, v in sorted(tfidfDictionary.items(), key = lambda x: -x[1] ):
         i += 0
-        if i > 10: break
-        print BKdictionary[k], ': ', v
+        if i < 10: print BKdictionary[k], ': ', v
+        
         
 if __name__ == "__main__":
-        getAllFiles()
-        writeToFile()
-        getQuery()
-        Calculate()
-##        print 'Total number of the term Informatics: ', numOfInformatics
-##        print 'Total number of the term Irvine: ', numOfIrvine
-##        print 'Total number of the term Mondego: ', numOfMondego
-##        print 'Number of Documents: ', numOfDocuments
-##        print 'Number of Unique Words: ', len(invertedIndex)
-##        print 'Total size(in KB): 88334 KB'
+    getAllFiles()
+    Calculate()
 
 
 
